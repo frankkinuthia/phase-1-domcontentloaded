d4c84e6e7e477792f0c9ce80845c244d79a222ff
+ <+>
2022-12-04 14:02:36 +0300

    Assignment done, revisit later

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const newTaskForm = document.getElementById("create-task-form");
+    const newTaskDesc = document.getElementById("new-task-description");
+    const newTaskPriority = document.getElementById("new-task-priority");
+
+    //creating the list where the new tasks will stay on the DOM
+
+    const newTaskUl = document.getElementById("tasks");
+
+    //attach the event listener
+
+    newTaskForm.addEventListener("submit", createNewTask);
+});
+
+const createNewTask = event => {
+    //stopping the default form submit behaviour 
+    event.preventDefault();
+
+    const newTaskDesc = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDesc.value;
+
+    appendNewTask(newTask);
+    event.target.reset();
+}
+
+const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task);
+}
